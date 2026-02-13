e8763e6342d478c4de7842a177c15d94a9e62f78
Sukhshum2628 <2022a1r002@mietjammu.in>
2026-02-13 11:08:46 +0530

    fix(native): remove forced cache clearing on startup

--- android/app/src/main/java/com/conslog/app/MainActivity.java
+++ android/app/src/main/java/com/conslog/app/MainActivity.java
@@ -5,14 +5,4 @@ import android.webkit.ValueCallback;
 import android.webkit.WebSettings;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {
-    @Override
-    public void onStart() {
-        super.onStart();
-        // Clear all WebView caches to break the stale PWA loop
-        if (this.bridge != null && this.bridge.getWebView() != null) {
-            this.bridge.getWebView().clearCache(true);
-            android.webkit.WebStorage.getInstance().deleteAllData();
-        }
-    }
-}
+public class MainActivity extends BridgeActivity {}
